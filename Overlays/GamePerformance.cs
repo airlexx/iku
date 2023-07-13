fb8b9a07970e71c0e9e2139a6c4569f212c85bd3
Airlex <contact@airlex.me>
2023-07-13 16:40:28 +0400

    feat: add FrameTime to GamePerformance overlay

--- Overlays/GamePerformance.cs
+++ Overlays/GamePerformance.cs
@@ -7,8 +7,25 @@ namespace iku.Game.Overlays;
 
 public static class GamePerformance
 {
+    private static int FrameRate;
+    private static float FrameTime;
+    private static double ElapsedTime;
+    private static float RefreshTime = 0.5f;
+
     public static void Show()
     {
-        Print.Info(Window.FrameRate.ToString(), new UnitPoint(0.90f, -0.90f));
+        float deltaTime = Window.FrameTime;
+        ElapsedTime += deltaTime;
+
+        if (ElapsedTime >= RefreshTime)
+        {
+            FrameRate = Window.FrameRate;
+            FrameTime = Window.FrameTime;
+
+            ElapsedTime = 0;
+        }
+
+        Print.Info($"{FrameRate} fps", new UnitPoint(0.80f, -0.70f));
+        Print.Info($"{Math.Round(FrameTime * 1000, 2)} ms", new UnitPoint(0.80f, -0.80f));
     }
 }
