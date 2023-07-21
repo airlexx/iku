79a2b53c42e19769eb7a27fb42bff4c62e5c033b
Airlex <contact@airlex.me>
2023-07-21 07:43:40 +0400

    feat: add new font

--- Graphics/Font.cs
+++ Graphics/Font.cs
@@ -5,9 +5,11 @@ namespace iku.Game.Graphics;
 
 public class Font
 {
-    public static Raylib_cs.Font fontChillaxMedium = Raylib.LoadFontEx("Resources/Fonts/Chillax/Chillax-Bold.ttf", 96, null, 0);
+    public static Raylib_cs.Font fontChillaxMedium = Raylib.LoadFontEx("Resources/Fonts/Chillax/Chillax-Medium.ttf", 96, null, 0);
 
     public static Raylib_cs.Font fontChillaxLight = Raylib.LoadFontEx("Resources/Fonts/Chillax/Chillax-Light.ttf", 96, null, 0);
+
+    public static Raylib_cs.Font fontChillaxBold = Raylib.LoadFontEx("Resources/Fonts/Chillax/Chillax-Bold.ttf", 300, null, 0);
 
     public static Raylib_cs.Font fontFiraCodeMedium = Raylib.LoadFontEx("Resources/Fonts/FiraCode/FiraCode-Medium.ttf", 96, null, 0);
 }
