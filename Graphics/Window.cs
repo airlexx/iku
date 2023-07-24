649fab7a16909d0ee834c42db69920e131fd3217
Airlex <contact@airlex.me>
2023-07-24 07:50:29 +0400

    feat: add fullscreen mode

--- Graphics/Window.cs
+++ Graphics/Window.cs
@@ -18,17 +18,27 @@ public struct Window
     public static int FrameLimit;
     public static float FrameTime;
     public static double RunningTime;
+    public static int CurrentMonitor;
+    public static int MonitorWidth;
+    public static int MonitorHeight;
 
     public Window()
     {
-        Width = 1920;
-        Height = 1080;
+        Width = 960;
+        Height = 720;
 
         Ratio = (float)Width / (float)Height;
         
-        FrameLimit = 60;
+        FrameLimit = 9999;
 
+        Logger.Info($"ikuzo!");
+
+        Raylib.SetConfigFlags(ConfigFlags.FLAG_WINDOW_RESIZABLE);
         Raylib.InitWindow(Width, Height, Name);
+        Raylib.SetWindowMinSize(720, 540);
+
+        if (Raylib.IsWindowReady())
+            Logger.Info($"Window has been initialized successfully");
 
         Image icon = Raylib.LoadImage("Assets/iku-desktop.png");
         Raylib.SetWindowIcon(icon);
@@ -38,9 +48,27 @@ public struct Window
 
     public static void Update()
     {
+        Width = Raylib.GetScreenWidth();
+        Height = Raylib.GetScreenHeight();
+
         FrameRate = Raylib.GetFPS();
         FrameTime = Raylib.GetFrameTime();
+
         RunningTime = Raylib.GetTime();
+
+        CurrentMonitor = Raylib.GetCurrentMonitor();
+        MonitorWidth = Raylib.GetMonitorWidth(CurrentMonitor);
+        MonitorHeight = Raylib.GetMonitorHeight(CurrentMonitor);
+
+        if (Raylib.IsKeyPressed(KeyboardKey.KEY_F11))
+        {
+            Raylib.SetWindowSize(MonitorWidth, MonitorHeight);
+            Raylib.ToggleFullscreen();
+            Raylib.SetWindowPosition(100, 100);
+        }
+
+        if (Raylib.IsWindowResized())
+            Logger.Info($"Window resized to {Width}x{Height}");
     }
 
     public static void Close()
