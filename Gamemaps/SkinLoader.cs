0db7efb48848c8972c6614bc1e4f7f572cfd17ca
Airlex <contact@airlex.me>
2023-08-07 23:44:55 +0400

    refactor: change properties name

--- Gamemaps/SkinLoader.cs
+++ Gamemaps/SkinLoader.cs
@@ -9,10 +9,10 @@ public static class SkinLoader
 {
     private const string SkinDir = "Skins/iku";
 
-    private static readonly Texture2D[] hitPointRight = new Texture2D[3];
-    private static readonly Texture2D[] hitPointLeft = new Texture2D[3];
-    private static readonly Texture2D[] hitPointUp = new Texture2D[3];
-    private static readonly Texture2D[] hitPointDown = new Texture2D[3];
+    public static readonly Texture2D[] HitPointRight = new Texture2D[3];
+    public static readonly Texture2D[] HitPointLeft = new Texture2D[3];
+    public static readonly Texture2D[] HitPointUp = new Texture2D[3];
+    public static readonly Texture2D[] HitPointDown = new Texture2D[3];
 
     public static void Load()
     {
@@ -33,21 +33,20 @@ public static class SkinLoader
         Image hitPointU2 = Raylib.LoadImage(Path.Combine(dir, "hitpoint-up-2.png"));
         Image hitPointD2 = Raylib.LoadImage(Path.Combine(dir, "hitpoint-down-2.png"));
 
+        HitPointRight[0] = Raylib.LoadTextureFromImage(hitPointR0);
+        HitPointLeft[0] = Raylib.LoadTextureFromImage(hitPointL0);
+        HitPointUp[0] = Raylib.LoadTextureFromImage(hitPointU0);
+        HitPointDown[0] = Raylib.LoadTextureFromImage(hitPointD0);
 
-        hitPointRight[0] = Raylib.LoadTextureFromImage(hitPointR0);
-        hitPointLeft[0] = Raylib.LoadTextureFromImage(hitPointL0);
-        hitPointUp[0] = Raylib.LoadTextureFromImage(hitPointU0);
-        hitPointDown[0] = Raylib.LoadTextureFromImage(hitPointD0);
+        HitPointRight[1] = Raylib.LoadTextureFromImage(hitPointR1);
+        HitPointLeft[1] = Raylib.LoadTextureFromImage(hitPointL1);
+        HitPointUp[1] = Raylib.LoadTextureFromImage(hitPointU1);
+        HitPointDown[1] = Raylib.LoadTextureFromImage(hitPointD1);
 
-        hitPointRight[1] = Raylib.LoadTextureFromImage(hitPointR1);
-        hitPointLeft[1] = Raylib.LoadTextureFromImage(hitPointL1);
-        hitPointUp[1] = Raylib.LoadTextureFromImage(hitPointU1);
-        hitPointDown[1] = Raylib.LoadTextureFromImage(hitPointD1);
-
-        hitPointRight[2] = Raylib.LoadTextureFromImage(hitPointR2);
-        hitPointLeft[2] = Raylib.LoadTextureFromImage(hitPointL2);
-        hitPointUp[2] = Raylib.LoadTextureFromImage(hitPointU2);
-        hitPointDown[2] = Raylib.LoadTextureFromImage(hitPointD2);
+        HitPointRight[2] = Raylib.LoadTextureFromImage(hitPointR2);
+        HitPointLeft[2] = Raylib.LoadTextureFromImage(hitPointL2);
+        HitPointUp[2] = Raylib.LoadTextureFromImage(hitPointU2);
+        HitPointDown[2] = Raylib.LoadTextureFromImage(hitPointD2);
 
         Raylib.UnloadImage(hitPointR0);
         Raylib.UnloadImage(hitPointL0);
@@ -63,23 +62,5 @@ public static class SkinLoader
         Raylib.UnloadImage(hitPointL2);
         Raylib.UnloadImage(hitPointU2);
         Raylib.UnloadImage(hitPointD2);
-    }
-
-    public static void DrawPoint(HitPointAction action, byte status, Vector2 position, float size)
-    {
-        Rectangle recSource = new(0, 0, 256, 256);
-        Rectangle recDest = new(position.X, position.Y, size, size);
-        Vector2 origin = new(size / 2, size / 2);
-
-        var hitPointTexture = action switch
-        {
-            HitPointAction.Right => hitPointRight[status],
-            HitPointAction.Left => hitPointLeft[status],
-            HitPointAction.Up => hitPointUp[status],
-            HitPointAction.Down => hitPointDown[status],
-            _ => hitPointDown[0],
-        };
-
-        Raylib.DrawTexturePro(hitPointTexture, recSource, recDest, origin, 0f, Color.WHITE);
     }
 }
